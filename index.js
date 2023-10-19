6786723716c8f6c9315b76a4e1a0e1f0fbb565bb
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2023-10-19 11:35:27 +0600

    Implement delete functionality for cart items - Extraction of the id from the request parameters. - Construction of a query to match the item by its unique identifier. - Deletion of the item from the cart collection using MongoDB's deleteOne method. - Sending the result of the deletion operation as the response.

--- index.js
+++ index.js
@@ -73,6 +73,16 @@ async function run() {
       res.send(result)
     })
 
+    
+    // delete
+    app.delete('/cart/:id',async(req,res)=>{
+      const id=req.params.id 
+      console.log(id);
+      const query={_id:new ObjectId(id)}
+      const result=await cartCollection.deleteOne(query)
+      res.send(result)
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
