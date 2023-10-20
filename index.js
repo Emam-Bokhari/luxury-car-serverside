31d299e27836a21f19e47eaa23378c069105fbbc
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2023-10-20 17:18:51 +0600

    change the name of put req name products to productsData

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ async function run() {
 
 
     // update
-    app.put('/products/:id',async(req,res)=>{
+    app.put('/productsData/:id',async(req,res)=>{
       const id=req.params.id 
       console.log(id);
       const query={_id:new ObjectId(id)}
